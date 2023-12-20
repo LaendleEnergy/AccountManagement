b10f1417ab72b13ab275f7192b434fe5089bbc93
Felix Lahnsteiner <felix.lahnsteiner@outlook.com>
2023-12-20 21:44:22 +0100

    tests integration of householdservice

---
+++ src/test/java/at/fhv/master/laendleenergy/integration/household/PersistanceFunctionTests.java
@@ -0,0 +1,32 @@
+package at.fhv.master.laendleenergy.integration.household;
+
+import at.fhv.master.laendleenergy.application.HouseholdService;
+import at.fhv.master.laendleenergy.domain.ElectricityPricingPlan;
+import at.fhv.master.laendleenergy.view.DTOs.CreateHouseholdDTO;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static org.wildfly.common.Assert.assertFalse;
+
+@QuarkusTest
+public class PersistanceFunctionTests {
+
+    @Inject
+    HouseholdService householdService;
+
+    @Test
+    public void testInsertNewHouseholdShouldWork() {
+        CreateHouseholdDTO dto = new CreateHouseholdDTO(
+                "deviceId",
+                "emailAddress",
+                "password",
+                "name",
+                ElectricityPricingPlan.NORMAL.getName()
+        );
+
+        String result = householdService.createHousehold(dto);
+
+        assertFalse(result.isEmpty());
+    }
+}
