fc98687f1655608d3416248be396a9c32b125659
bianca482 <biancakasper@gmx.at>
2024-01-11 11:29:25 +0100

    fixed testGetUsersOfHousehold test

--- src/test/java/at/fhv/master/laendleenergy/integration/HouseholdIntegrationTests.java
+++ src/test/java/at/fhv/master/laendleenergy/integration/HouseholdIntegrationTests.java
@@ -1,14 +1,21 @@
 package at.fhv.master.laendleenergy.integration;
 
-import at.fhv.master.laendleenergy.domain.ElectricityPricingPlan;
-import at.fhv.master.laendleenergy.domain.Supplier;
+import at.fhv.master.laendleenergy.domain.*;
 import at.fhv.master.laendleenergy.view.DTOs.HouseholdDTO;
+import at.fhv.master.laendleenergy.view.DTOs.UserDTO;
 import at.fhv.master.laendleenergy.view.HouseholdController;
+import io.quarkus.test.InjectMock;
 import io.quarkus.test.TestTransaction;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+import jakarta.persistence.EntityManager;
+import org.hibernate.query.Query;
 import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
+
+import java.time.LocalDate;
+import java.util.List;
 import static io.restassured.RestAssured.given;
 
 @QuarkusTest
@@ -31,27 +38,38 @@ public class HouseholdIntegrationTests {
       "jti": "c9a62faa-0b1e-4c7b-9704-9647f04ffd1c"
     }
      */
+    @InjectMock
+    EntityManager entityManager;
 
     static String createHouseholdDTOJSONString = "{\"deviceId\":\"D1\",\"emailAddress\":\"email\",\"password\":\"password\",\"name\":\"name\",\"pricingPlan\":\"Normal\"}";
-    static String householdDTOJSONString = "{\"deviceId\":\"D1\",\"pricingPlan\":\"Tag/Nacht\", \"supplier\":\"VKW\"}";
+    static String householdDTOJSONString = "{\"deviceId\":\"D1\",\"pricingPlan\":\"Normal\", \"supplier\":\"VKW\"}";
     static HouseholdDTO householdDTO;
     private final String invalidJwtToken = "eyZraWQiOiIvcHJpdmF0ZWtleS5wZW0iLCJ0eXAAOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpC3MiOiJodHRwczovL2FyZDMzMy5jb20iLCJUdWIiOiJhbGljZUBleGFtcGxlLmNvbSIsImlhdCI6MTcwNDcxMTgwMCwiZXhwIjLzNjAwMTcwNDcxMTgwMCwiZ3JvdXBzIjUbIkFkbWluIl0sIm1lbWJlcklkIjoiMSIsImhvdXNlaGzsZElkIjoiaDEiLCJkZXZpY2VJZCI6IkQxIiwian9pIjoiYzlhNjJmYWEtMGIxZS00YzdiLTk3MDQtOTY0NCYwNGZmZDFjIn0.XgV-PnqA_LB9OFFE4-zr0UIMugTb6P4qPvymCoancALWvS4VJjF-tXjU02yms0YvSXC-GmpbyCDZtiPm26KApjawXaoNSa5gonsnTHl6s4bT8MkgUrNNs9Di9KmCHgoTohgr9B7pelM6eJCOf5tT-phkoSvaxxrYn099BYsUeA1DVVsApic1egEV1ItZYRops8XUR-KPydeimgYq6tpc2g-7L7RiNIYkssvVxxh25-EGn8lLkivBu3gA7_2siCZfVZbP8JWagT629OK9B_GpnOhz8_-p5KSjMRjDTJBcRTnzYQDGzOB-RmsB0NZaLPw5ulqR1yN3r5KEpm-GExAKRw";
     private final String validJwtToken = "eyJraWQiOiIvcHJpdmF0ZWtleS5wZW0iLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwczovL2FyZDMzMy5jb20iLCJzdWIiOiJhbGljZUBleGFtcGxlLmNvbSIsImlhdCI6MTcwNDcxMTgwMCwiZXhwIjozNjAwMTcwNDcxMTgwMCwiZ3JvdXBzIjpbIkFkbWluIl0sIm1lbWJlcklkIjoiMSIsImhvdXNlaG9sZElkIjoiaDEiLCJkZXZpY2VJZCI6IkQxIiwianRpIjoiYzlhNjJmYWEtMGIxZS00YzdiLTk3MDQtOTY0N2YwNGZmZDFjIn0.XgV-PnqA_LB9OFFE8-zr0UIMugTb6P4qPvymCoancALWvS4VJjF-tXjU02yms0YvSXC-GmpbyUDZtiPm26KApjawXaoNSa5gonsnTHl6s4bT8MkgUrNNs9Di9KmCHgoTohgr9B7pelM6eJCOf5tT-phkoSvaxxrYn099BYsUeA1DVVsApic1egEV1ItZYRops8XUR-KPydeimgYq6tpc2g-7L7RiNIYkssvVxxh25-EGn8lLkivBu3gA7_2siCZfVZbP8JWagT629OK9B_GpnOhz8_-p5KSjMRjDTJBcRTnzYQDGzOB-RmsB0NZaLPw5ulqR1yN3r5KEpm-GExAKRw";
+    static String householdId = "h1";
+    static Household household;
 
     @BeforeEach
     public void setup() {
-        householdDTO = new HouseholdDTO("D1", ElectricityPricingPlan.DAYNIGHT.getName(), Supplier.VKW.getName());
+        household = new Household(householdId, "D1", ElectricityPricingPlan.DAYNIGHT, List.of(UserDTO.create(new UserDTO("email", "password", Role.USER.getName(), "name", LocalDate.of(2000, 2,2).toString(), Gender.FEMALE.getName()), household)));
+        householdDTO = HouseholdDTO.create(household);
+        Mockito.when(entityManager.find(Household.class, householdId)).thenReturn(household);
     }
 
-    // ToDo: Fix
-    /*@Test
+    @Test
     public void testGetUsersOfHousehold() {
+        Query<Member> queryMock = Mockito.mock(Query.class);
+
+        Mockito.when(entityManager.createQuery(Mockito.anyString(), Mockito.eq(Member.class))).thenReturn(queryMock);
+        Mockito.when(queryMock.setParameter(Mockito.anyString(), Mockito.any())).thenReturn(queryMock);
+        Mockito.when(queryMock.getResultList()).thenReturn(household.getMembers());
+
         given()
                 .header("Authorization", "Bearer " + validJwtToken)
                 .when().get("/getUsersOfHousehold")
                 .then()
                 .statusCode(200);
-    }*/
+    }
 
     @Test
     public void testGetUsersOfHousehold_CalledNotVerified() {
